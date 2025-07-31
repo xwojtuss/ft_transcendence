5a465da1e4a237e7d990a6c7d785ae8b920f4471
Wojtek Kornatowski <wojkor338@gmail.com>
2025-08-01 01:28:30 +0200

    basic sketch done, changed alt in login and register

--- frontend/ts/login-register-form.ts
+++ frontend/ts/login-register-form.ts
@@ -7,12 +7,12 @@ export default function changePasswordButton(image: HTMLElement, passwordField: 
     if (imageElement.classList.contains('button-on')) {
         imageElement.classList.remove('button-on');
         imageElement.src = 'https://img.icons8.com/material-outlined/24/visible--v1.png';
-        imageElement.alt = 'visible--v1';
+        imageElement.alt = 'Show password';
         passwordField.type = 'password';
     } else {
         imageElement.classList.add('button-on');
         imageElement.src = 'https://img.icons8.com/material-outlined/24/hide.png';
-        imageElement.alt = 'hide';
+        imageElement.alt = 'Hide password';
         passwordField.type = 'text';
     }
 }
